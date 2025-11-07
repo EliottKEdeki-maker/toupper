c007983b67044dd16347f25fc01126638014d330
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-07 14:39:49 -0500

    Update and rename Input to input.v

--- input.v
+++ input.v
@@ -1,4 +1,4 @@
-module num(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p); // Set up 8 inputs and outputs for digit you input and output of toupper function
+module num (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p); // Set up 8 inputs and outputs for digit you input and output of toupper function
   `timescale 1ns / 1ps
   input a, b, c, d, e, f, g, h;
   output i, j, k, l, m, n, o, p;
