3f4527d2402f78a5c1a32dc0c23e217d3e377e4a
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-12 11:53:46 -0500

    Fix syntax errors and update initial values

--- output.v
+++ output.v
@@ -1,33 +1,34 @@
 module tb;
   `timescale 1ns / 1ps
   wire W1, W2, W3, W4, W5, W6, W7, W8;     // 8 wires for 8 outputs 
-  reg A,B,C,D,E,F,G,H       // 8 regs for 8 inputs
+  reg A,B,C,D,E,F,G,H ;      // 8 regs for 8 inputs
   num N1(A,B,C,D,E,F,G,H,W1,W2,W3,W4,W5,W6,W7,W8);
 
 initial 
   begin
     $dumpfile("wave.vcd")
     $dumpvars(0, tb,num);
-    A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b1 C = 1'b0 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
-    #100  A = 1'b0 B = 1'b0 C = 1'b1 D = 1'b0 E = 1'b1 F = 1'b0 G = 1'b0 H = 1'b0
+    A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b0; H = 1'b0;
+    #100 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b0; H = 1'b0;
+    #100 A = 1'b1; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b0; F = 1'b1; G = 1'b1; H = 1'b1;
+    #100 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b1; H = 1'b1;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b1; G = 1'b0; H = 1'b0;
+    #100 A = 1'b0; B = 1'b0; C = 1'b0; D = 1'b1; E = 1'b0; F = 1'b1; G = 1'b0; H = 1'b0;
+    #100 A = 1'b1; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b1;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b1;
+    #100 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b1;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b0;
+    #100 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b1; G = 1'b1; H = 1'b1;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b1; G = 1'b0; H = 1'b1;
+    #100 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b1; E = 1'b0; F = 1'b0; G = 1'b1; H = 1'b0;
+    #100 A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b0;
+    #100 A = 1'b1; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b1; F = 1'b1; G = 1'b1; H = 1'b1;
+    #100 A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b0;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b1;
+    #100 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b1; G = 1'b0; H = 1'b0;
+    #100 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b1; G = 1'b1; H = 1'b1;
+    
   end
-  initial #250 $finish;
+  initial #200 $finish;
 endmodule
  
