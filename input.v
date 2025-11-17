ee7376d986f4d25f6b47bd3ed86f006f84df0b4b
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-17 12:01:30 -0500

    Refactor logic for signals j and k in input.v

--- input.v
+++ input.v
@@ -4,14 +4,12 @@ module num (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p); // Set up 8 inputs and outputs for
   
   input a, b, c, d, e, f, g, h;
   output i, j, k, l, m, n, o, p;
-  wire w1, w2, w3, w4;
+  wire w1;
   
-  and #(10) (w1, b, c); // j is only 1 if both b and c and are 1 so I use b and c to find j
-  buf #(4) (j, w1);
+  and #(10) (j, b, c); // j is only 1 if both b and c and are 1 so I use b and c to find j
 
-  xor #(15) (w3, b, c);// k is only true for b = 1 and c = 0 so I use xnor to get rid of 00 and 11 then check for b = 1 to get b = 1 and c = 0 
-  and #(10) (w4, w3, b);
-  buf #(4) (k, w4);
+  xor #(15) (w1, b, c);// k is only true for b = 1 and c = 0 so I use xnor to get rid of 00 and 11 then check for b = 1 to get b = 1 and c = 0 
+  and #(10) (k, w1, b);
 
   buf #(4) (i, a); // buff for these values since they never change since we are binary subtracting 32 which is 100000
   buf #(4) (l, d);
