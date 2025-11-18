ccc64e0e15831bf5cca436de7f1b4fb7f68fa99b
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-18 18:24:03 -0500

    Refactor testbench to use updated signal names

--- testbed.v
+++ testbed.v
@@ -1,35 +1,35 @@
 `timescale 1ns / 1ps
 module tb;
   
-  wire W1, W2, W3, W4, W5, W6, W7, W8;     // 8 wires for 8 outputs 
-  reg A,B,C,D,E,F,G,H ;      // 8 regs for 8 inputs
-  num N1(A,B,C,D,E,F,G,H,W1,W2,W3,W4,W5,W6,W7,W8);
+  wire o0, o1, o2, o3, o4, o5, o6, o7;     // 8 wires for 8 outputs 
+  reg i0,i1,i2,i3,i4,i5,i6,i7;      // 8 regs for 8 inputs
+  num N1(i0,i1,i2,i3,i4,i5,i6,i7,o0,o1,o2,o3,o4,o5,o6,o7);
 
-initial 
+initial //50 as arbitrary inter-input delay to begin with before we find threshold
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tb, N1);
-    A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b0; H = 1'b0;
-    #50 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b0; H = 1'b0;
-    #50 A = 1'b1; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b0; F = 1'b1; G = 1'b1; H = 1'b1;
-    #50 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b1; H = 1'b1;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b1; G = 1'b0; H = 1'b0;
-    #50 A = 1'b0; B = 1'b0; C = 1'b0; D = 1'b1; E = 1'b0; F = 1'b1; G = 1'b0; H = 1'b0;
-    #50 A = 1'b1; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b1;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b1;
-    #50 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b1;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b0;
-    #50 A = 1'b0; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b1; G = 1'b1; H = 1'b1;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b0; E = 1'b1; F = 1'b1; G = 1'b0; H = 1'b1;
-    #50 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b1; E = 1'b0; F = 1'b0; G = 1'b1; H = 1'b0;
-    #50 A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b0; F = 1'b0; G = 1'b0; H = 1'b0;
-    #50 A = 1'b1; B = 1'b1; C = 1'b0; D = 1'b0; E = 1'b1; F = 1'b1; G = 1'b1; H = 1'b1;
-    #50 A = 1'b0; B = 1'b0; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b0;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b0; G = 1'b1; H = 1'b1;
-    #50 A = 1'b1; B = 1'b0; C = 1'b0; D = 1'b0; E = 1'b0; F = 1'b1; G = 1'b0; H = 1'b0;
-    #50 A = 1'b0; B = 1'b1; C = 1'b1; D = 1'b1; E = 1'b1; F = 1'b1; G = 1'b1; H = 1'b1;
+    i0 = 1'b0; i1 = 1'b0; i2 = 1'b1; i3 = 1'b0; i4 = 1'b1; i5 = 1'b0; i6 = 1'b0; i7 = 1'b0; //
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b0; i3 = 1'b0; i4 = 1'b1; i5 = 1'b0; i6 = 1'b0; i7 = 1'b0;
+    #50 i0 = 1'b1; i1 = 1'b0; i2 = 1'b1; i3 = 1'b1; i4 = 1'b0; i5 = 1'b1; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b1; i1 = 1'b0; i2 = 1'b0; i3 = 1'b0; i4 = 1'b0; i5 = 1'b0; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b1; i4 = 1'b1; i5 = 1'b1; i6 = 1'b0; i7 = 1'b0;
+    #50 i0 = 1'b0; i1 = 1'b0; i2 = 1'b0; i3 = 1'b1; i4 = 1'b0; i5 = 1'b1; i6 = 1'b0; i7 = 1'b0;
+    #50 i0 = 1'b1; i1 = 1'b1; i2 = 1'b1; i3 = 1'b0; i4 = 1'b1; i5 = 1'b0; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b0; i4 = 1'b0; i5 = 1'b0; i6 = 1'b0; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b0; i3 = 1'b0; i4 = 1'b0; i5 = 1'b0; i6 = 1'b0; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b1; i4 = 1'b1; i5 = 1'b0; i6 = 1'b1; i7 = 1'b0;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b0; i3 = 1'b0; i4 = 1'b0; i5 = 1'b1; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b0; i4 = 1'b1; i5 = 1'b1; i6 = 1'b0; i7 = 1'b1;
+    #50 i0 = 1'b1; i1 = 1'b0; i2 = 1'b0; i3 = 1'b1; i4 = 1'b0; i5 = 1'b0; i6 = 1'b1; i7 = 1'b0;
+    #50 i0 = 1'b0; i1 = 1'b0; i2 = 1'b1; i3 = 1'b1; i4 = 1'b0; i5 = 1'b0; i6 = 1'b0; i7 = 1'b0;
+    #50 i0 = 1'b1; i1 = 1'b1; i2 = 1'b0; i3 = 1'b0; i4 = 1'b1; i5 = 1'b1; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b0; i1 = 1'b0; i2 = 1'b1; i3 = 1'b1; i4 = 1'b1; i5 = 1'b0; i6 = 1'b1; i7 = 1'b0;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b1; i4 = 1'b1; i5 = 1'b0; i6 = 1'b1; i7 = 1'b1;
+    #50 i0 = 1'b1; i1 = 1'b0; i2 = 1'b0; i3 = 1'b0; i4 = 1'b0; i5 = 1'b1; i6 = 1'b0; i7 = 1'b0;
+    #50 i0 = 1'b0; i1 = 1'b1; i2 = 1'b1; i3 = 1'b1; i4 = 1'b1; i5 = 1'b1; i6 = 1'b1; i7 = 1'b1;
     
   end
-  initial #950 $finish;
+  initial #950 $finish; //total time is 950 since 50 * 19 = 950 which is the total time needed for each input
 endmodule
  
