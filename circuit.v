29a0605651ffc340d7be8658b6ce1cf1f62f4058
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-18 18:43:12 -0500

    Reorder input and output ports in num module

--- circuit.v
+++ circuit.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
-module num (i0,i1,i2,i3,i4,i5,i6,i7,o0,o1,o2,o3,o4,o5,o6,o7); // Set up 8 inputs and outputs for digit you input and output of toupper function
+module num (i7,i6,i5,i4,i3,i2,i1,i0,o7,o6,o5,o4,o3,o2,o1,o0); // Set up 8 inputs and outputs for digit you input and output of toupper function
   
   
-  input i0, i1, i2, i3, i4, i5, i6, i7;
-  output o0, o1, o2, o3, o4, o5, o6, o7;
+  input i7,i6,i5,i4,i3,i2,i1,i0;
+  output o7,o6,o5,o4,o3,o2,o1,o0;
   wire w1;
   
-  and #(10) (o1, i1, i2); // o1 is only 1 if both i1 and i2 and are 1 so I use i1 and i2 to find o2
+  and #(10) (o6, i6, i5); // o1 is only 1 if both i6 and i5 and are 1 so I use i6 AND i5 to find o1
 
-  xor #(15) (w1, i1, i2);// o2 is only true for i1 = 1 and i2 = 0 so I use xnor to get rid of 00 and 11 then check for i1 = 1 to get i1 = 1 and i2 = 0 
-  and #(10) (o2, w1, i1);
+  xor #(15) (w1, i6, i5);// o5 is only true for i6 = 1 and i5 = 0 so I use xnor to get rid of 00 and 11 then check for i6 = 1 to get i6 = 1 and i5 = 0 
+  and #(10) (o5, w1, i6);
 
   buf #(4) (o0, i0); // buf for these values since they never change since we are binary subtracting 32 which is 100000
   buf #(4) (o3, i3);
