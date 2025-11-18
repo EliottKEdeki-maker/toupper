415da793eaabda36e732fdca2d3ccf77c1ddf39a
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-11-18 18:21:41 -0500

    Clarify buffer comment in circuit.v
    Updated comment for clarity on buffer usage.

--- circuit.v
+++ circuit.v
@@ -11,7 +11,7 @@ module num (i0,i1,i2,i3,i4,i5,i6,i7,o0,o1,o2,o3,o4,o5,o6,o7); // Set up 8 inputs
   xor #(15) (w1, i1, i2);// o2 is only true for i1 = 1 and i2 = 0 so I use xnor to get rid of 00 and 11 then check for i1 = 1 to get i1 = 1 and i2 = 0 
   and #(10) (o2, w1, i1);
 
-  buf #(4) (o0, i0); // buff for these values since they never change since we are binary subtracting 32 which is 100000
+  buf #(4) (o0, i0); // buf for these values since they never change since we are binary subtracting 32 which is 100000
   buf #(4) (o3, i3);
   buf #(4) (o4, i4);
   buf #(4) (o5, i5);
